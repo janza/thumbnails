8ee73c7c033d2a165b1eedfa2accf84410e9522a
Josip Janzic <me@josip.dev>
2023-11-08 14:01:45 +0100

    Update prompt

--- src/app/thumbnails/route.ts
+++ src/app/thumbnails/route.ts
@@ -28,14 +28,14 @@ export async function POST(request: Request) {
   const props = {
     model: "dall-e-3",
     prompt: `
-A simple logo for the topic: ${topic}.
+A small logo for ${topic}.
 
-Using this color pallete: #faf6ee, #ffffff, rgb(253, 127, 127), #746bd3, #d6ccff. Do not output color pallete sample.
+Using this color pallete: #ffffff, #FD7F7F, #746bd3, #d6ccff. Do not output color pallete sample.
+Use #faf6ee as background color.
 
-It's very important to use simple lines and for the illustratio to not have a lot of detail.
-Do not output any text.
-
-The image has a slight isometric perspective, giving it a three-dimensional appearance while maintaining its illustrative flatness. There's a sense of whimsy in the drawing, with attention to the functional aspects, but not adhering strictly to realistic proportions or details.
+Simple
+No text.
+Isometric
 `,
     n: 1,
     size: "1024x1024",
